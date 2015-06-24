0b9cc435e082d070dbb97560edb21fdd0f0fccd8
Dovik Nissim <dovik@dovik-macbookpro.roam.corp.google.com>
2015-06-24 16:14:14 -0700

    CHANGE - making UI changes

--- WiseUI/wise/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from wise.models import triggers
-
-# Register your models here.
-admin.site.register(triggers)
--- WiseUI/wise/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import render_to_response
-from wise.models import triggers
-
-def home(request):
-	triggs = triggers.objects.all()[:10]
-	return render_to_response('index.html', {'triggs': triggs})
